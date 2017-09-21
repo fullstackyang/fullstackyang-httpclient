c134caa555a14ff77e7ccca92095f5609227c7e8
superyy@foxmail.com <superyy@foxmail.com>
2017-09-21 14:23:22 +0800

    reorgnized pom.xml

--- src/main/java/com/fullstackyang/httpclient/HttpRequestUtils.java
+++ src/main/java/com/fullstackyang/httpclient/HttpRequestUtils.java
@@ -1,13 +1,10 @@
 package com.fullstackyang.httpclient;
 
 import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
 import org.apache.http.Consts;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.client.methods.RequestBuilder;
 import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.cookie.BasicClientCookie;
 import org.apache.http.message.BasicNameValuePair;
@@ -18,7 +15,6 @@ import java.net.URISyntaxException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 /**
  * 辅助类，用于创建各种带参数，请求头等信息的http请求
